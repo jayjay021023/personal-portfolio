9ca78b93486cd837e5c8848e6f831c656290304d
jayjay021023 <azizaesonova767@gmail.com>
2026-09-18 12:17:29 +0500

    about me page

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 //==homepage
 //elementlarni topish
 const valueE1 = document.querySelector(".counter_val");
-const btnMinus = document.querySelector(".btn_minus");
-const btnPlus = document.querySelector(".btn_plus");
-const btnReset = document.querySelector(".btn_reset");
 
-let count = 12;
+if (valueE1) {
+  const btnMinus = document.querySelector(".btn_minus");
+  const btnPlus = document.querySelector(".btn_plus");
+  const btnReset = document.querySelector(".btn_reset");
 
-//===funksiyalar
-function updateDisplay() {
-  valueE1.textContent = count;
+  let count = 12;
+
+  //===funksiyalar
+  function updateDisplay() {
+    valueE1.textContent = count;
+  }
+
+  btnMinus.addEventListener("click", () => {
+    count--;
+    updateDisplay();
+  });
+  btnPlus.addEventListener("click", () => {
+    count++;
+    updateDisplay();
+  });
+
+  btnReset.addEventListener("click", () => {
+    count = 12;
+    updateDisplay();
+  });
 }
 
-btnPlus.addEventListener("click", () => {
-  count++;
-  updateDisplay();
-});
+//=====about page===belgilarni sanash===
+const textInput = document.getElementById("textInput");
+const charCount = document.getElementById("charCount");
 
-btnMinus.addEventListener("click", () => {
-  count--;
-  updateDisplay();
-});
-
-btnReset.addEventListener("click", () => {
-  count = 12;
-  updateDisplay();
-});
+if (textInput) {
+  textInput.addEventListener("input", () => {
+    charCount.textContent = textInput.value.length;
+  });
+}
